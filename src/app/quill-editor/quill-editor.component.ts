c7b3944ea4c8e42989bf453be5bde45f30e8650b
Jeff Wu <crazy.alu@outlook.com>
2023-08-03 00:09:12 +0800

    Setup the toobar options and implemant imageHandler
    # Conflicts:
    #	src/app/quill-editor/quill-editor.component.html
    #	src/app/quill-editor/quill-editor.component.ts

--- src/app/quill-editor/quill-editor.component.ts
+++ src/app/quill-editor/quill-editor.component.ts
@@ -1,6 +1,14 @@
 import Quill from 'quill';
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SafeUrl } from '@angular/platform-browser';
+
+interface IImageMeta {
+  type: string;
+  dataUrl: string;
+  blobUrl: SafeUrl;
+  file: File | null;
+}
 
 @Component({
   selector: 'app-quill-editor',
@@ -14,16 +22,53 @@ export class QuillEditorComponent implements AfterViewInit {
 
   private quillEditor: any;
   content: string = ''; // 初始化編輯器內容為空字串
-
+  image: IImageMeta = {
+    type: '',
+    dataUrl: '',
+    blobUrl: '',
+    file: null,
+  };
+  constructor() {}
   ngAfterViewInit() {
-    this.quillEditor = new Quill(this.quillContainer.nativeElement, {
-      // this.quillEditor = new Quill("#quill", { // also works
-      theme: 'snow', // 可以選擇不同的主題，例如 'bubble' 或 'core'
-    });
-
+    this.initQuillEditor();
     // 監聽編輯器內容變化事件，並將變化同步到 Angular 的資料模型
     this.quillEditor.on('text-change', () => {
       this.content = this.quillEditor.root.innerHTML;
     });
+  }
+
+  initQuillEditor(): void {
+    const toolbarOptions = {
+      container: [
+        ['bold', 'italic', 'underline', 'strike'], // 預設的工具按鈕
+        [{ header: 1 }, { header: 2 }], // 預設的工具按鈕
+        [{ list: 'ordered' }, { list: 'bullet' }], // 預設的工具按鈕
+        [{ script: 'sub' }, { script: 'super' }], // 預設的工具按鈕
+        [{ indent: '-1' }, { indent: '+1' }], // 預設的工具按鈕
+        [{ direction: 'rtl' }], // 預設的工具按鈕
+        [{ size: ['small', false, 'large', 'huge'] }], // false 是 normal 的意思
+        [{ header: [1, 2, 3, 4, 5, 6, false] }], // 預設的工具按鈕
+        [{ color: [] }, { background: [] }], // 預設的工具按鈕
+        [{ font: [] }], // 預設的工具按鈕
+        ['image', 'customButton'], // 自定義的內建工具按鈕
+        ['clean'], // 預設的工具按鈕
+      ],
+      handlers: {
+        customButton: () => console.log('handle custom button'),
+      },
+    };
+    const icons = Quill.import('ui/icons');
+    icons['customButton'] = '<i class="fa-regular fa-star"></i>';
+    this.quillEditor = new Quill(this.quillContainer.nativeElement, {
+      // Quill Editor 的配置
+      theme: 'snow', // 可以選擇不同的主題，例如 'bubble' 或 'core'
+      modules: {
+        toolbar: toolbarOptions,
+      },
+    });
+  }
+
+  getQuillEditorInstance(): Quill {
+    return this.quillEditor!;
   }
 }
