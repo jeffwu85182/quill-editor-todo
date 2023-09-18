02be9cb83980de05be19c3fea32c44e68b34e7aa
Jeff Wu <crazy.alu@outlook.com>
2023-09-18 21:45:52 +0800

    Change to use standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { QuillEditorComponent } from './quill-editor/quill-editor.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  styleUrls: ['./app.component.scss'],
+  imports: [CommonModule, QuillEditorComponent],
 })
 export class AppComponent {
   title = 'quill-editor-todo';
