e2cc88d551c9835a607337c9c6a1585639c694ce
Jeff Wu <crazy.alu@outlook.com>
2023-09-21 22:20:07 +0800

    Use HTML to customize toolbar

--- src/app/quill-editor/quill-editor.component.ts
+++ src/app/quill-editor/quill-editor.component.ts
@@ -19,7 +19,7 @@ interface IImageMeta {
 })
 export class QuillEditorComponent implements AfterViewInit {
   @ViewChild('quillContainer') quillContainer!: ElementRef;
-
+  @ViewChild('toolbar') toolbar!: ElementRef;
   private quillEditor: any;
   content: string = ''; // 初始化編輯器內容為空字串
   image: IImageMeta = {
@@ -38,32 +38,38 @@ export class QuillEditorComponent implements AfterViewInit {
   }
 
   initQuillEditor(): void {
-    const toolbarOptions = {
-      container: [
-        ['bold', 'italic', 'underline', 'strike'], // 預設的工具按鈕
-        [{ header: 1 }, { header: 2 }], // 預設的工具按鈕
-        [{ list: 'ordered' }, { list: 'bullet' }], // 預設的工具按鈕
-        [{ script: 'sub' }, { script: 'super' }], // 預設的工具按鈕
-        [{ indent: '-1' }, { indent: '+1' }], // 預設的工具按鈕
-        [{ direction: 'rtl' }], // 預設的工具按鈕
-        [{ size: ['small', false, 'large', 'huge'] }], // false 是 normal 的意思
-        [{ header: [1, 2, 3, 4, 5, 6, false] }], // 預設的工具按鈕
-        [{ color: [] }, { background: [] }], // 預設的工具按鈕
-        [{ font: [] }], // 預設的工具按鈕
-        ['image', 'customButton'], // 自定義的內建工具按鈕
-        ['clean'], // 預設的工具按鈕
-      ],
-      handlers: {
-        customButton: () => console.log('handle custom button'),
-      },
-    };
+    // const toolbarOptions = {
+    //   container: [
+    //     ['bold', 'italic', 'underline', 'strike'], // 預設的工具按鈕
+    //     [{ header: 1 }, { header: 2 }], // 預設的工具按鈕
+    //     [{ list: 'ordered' }, { list: 'bullet' }], // 預設的工具按鈕
+    //     [{ script: 'sub' }, { script: 'super' }], // 預設的工具按鈕
+    //     [{ indent: '-1' }, { indent: '+1' }], // 預設的工具按鈕
+    //     [{ direction: 'rtl' }], // 預設的工具按鈕
+    //     [{ size: ['small', false, 'large', 'huge'] }], // false 是 normal 的意思
+    //     [{ header: [1, 2, 3, 4, 5, 6, false] }], // 預設的工具按鈕
+    //     [{ color: [] }, { background: [] }], // 預設的工具按鈕
+    //     [{ font: [] }], // 預設的工具按鈕
+    //     ['image', 'customButton'], // 自定義的內建工具按鈕
+    //     ['clean'], // 預設的工具按鈕
+    //   ],
+    //   handlers: {
+    //     customButton: () => console.log('handle custom button'),
+    //   },
+    // };
     const icons = Quill.import('ui/icons');
     icons['customButton'] = '<i class="fa-regular fa-star"></i>';
     this.quillEditor = new Quill(this.quillContainer.nativeElement, {
       // Quill Editor 的配置
       theme: 'snow', // 可以選擇不同的主題，例如 'bubble' 或 'core'
       modules: {
-        toolbar: toolbarOptions,
+        // toolbar: toolbarOptions,
+        toolbar: {
+          container: this.toolbar.nativeElement,
+          handlers: {
+            customButton: () => console.log('handle custom button'),
+          },
+        },
       },
     });
   }
